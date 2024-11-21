01fb4ff8eb03608038003cd44900838cd43d12ef
rlghks-aiofarm <rlghks@aiofarm.co.kr>
2024-11-21 13:22:54 +0900

    first commit

--- integration_tests/test_integration.py
+++ integration_tests/test_integration.py
@@ -12,21 +12,8 @@ repo_paths = {
     'repo2': repo2_path
 }
 
-# repo2 경로를 sys.path에 먼저 추가
-sys.path.insert(0, repo_paths['repo2'])
-print("sys.path after adding repo2:", sys.path)  # 경로 추가 후 상태 출력
-
 from repo2 import utils as repo2_utils
-sys.path.pop(0)  # repo2 경로를 제거
-print("sys.path after removing repo2:", sys.path)  # 경로 제거 후 상태 출력
-
-# repo1 경로를 sys.path에 추가
-sys.path.insert(0, repo_paths['repo1'])
-print("sys.path after adding repo1:", sys.path)  # 경로 추가 후 상태 출력
-
 from repo1 import utils as repo1_utils
-sys.path.pop(0)  # repo1 경로를 제거
-print("sys.path after removing repo1:", sys.path) 
 
 class TestIntegration(unittest.TestCase):
     def test_process_and_transform(self):
