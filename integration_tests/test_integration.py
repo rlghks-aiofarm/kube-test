ba31ced75c049d2cc9babf11b3f668f9ae96cab3
rlghks-aiofarm <rlghks@aiofarm.co.kr>
2024-11-21 11:20:56 +0900

    first commit

--- integration_tests/test_integration.py
+++ integration_tests/test_integration.py
@@ -6,17 +6,19 @@ import unittest
 repo1_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../repo1'))
 repo2_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../repo2'))
 
-sys.path.insert(0, repo1_path)  # repo1 경로 추가
-import utils as repo1_utils
-sys.path.pop(0)  # repo1 경로를 제거
 
 sys.path.insert(0, repo2_path)  # repo2 경로 추가
 import utils as repo2_utils
 sys.path.pop(0)  # repo2 경로를 제거
 
+sys.path.insert(0, repo1_path)  # repo1 경로 추가
+import utils as repo1_utils
+sys.path.pop(0)  # repo1 경로를 제거
 
 class TestIntegration(unittest.TestCase):
     def test_process_and_transform(self):
+        print("repo1_path:", repo1_path)
+        print("repo2_path:", repo2_path)
         print(dir(repo1_utils))
         processed = repo1_utils.process_data("integration test")
         print(dir(repo2_utils))
