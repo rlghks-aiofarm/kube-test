0550bb67c35e5e4ba7ec73ba21bf8f01850958b2
rlghks-aiofarm <rlghks@aiofarm.co.kr>
2024-11-21 11:24:19 +0900

    first commit

--- integration_tests/test_integration.py
+++ integration_tests/test_integration.py
@@ -7,13 +7,26 @@ repo1_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../repo1')
 repo2_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../repo2'))
 
 
-sys.path.insert(0, repo2_path)  # repo2 경로 추가
+repo_paths = {
+    'repo1': repo1_path,
+    'repo2': repo2_path
+}
+
+# repo2 경로를 sys.path에 먼저 추가
+sys.path.insert(0, repo_paths['repo2'])
+print("sys.path after adding repo2:", sys.path)  # 경로 추가 후 상태 출력
+
 import utils as repo2_utils
 sys.path.pop(0)  # repo2 경로를 제거
+print("sys.path after removing repo2:", sys.path)  # 경로 제거 후 상태 출력
 
-sys.path.insert(0, repo1_path)  # repo1 경로 추가
+# repo1 경로를 sys.path에 추가
+sys.path.insert(0, repo_paths['repo1'])
+print("sys.path after adding repo1:", sys.path)  # 경로 추가 후 상태 출력
+
 import utils as repo1_utils
 sys.path.pop(0)  # repo1 경로를 제거
+print("sys.path after removing repo1:", sys.path) 
 
 class TestIntegration(unittest.TestCase):
     def test_process_and_transform(self):
