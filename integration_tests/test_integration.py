e2d9bede5fda41b86a0b9fce02b6457d140af46d
rlghks-aiofarm <rlghks@aiofarm.co.kr>
2024-11-26 10:40:26 +0900

    test update

--- integration_tests/test_integration.py
+++ integration_tests/test_integration.py
@@ -6,14 +6,14 @@ import unittest
 repo1_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../repo1'))
 repo2_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../repo2'))
 
-sys.path.insert(0, repo1_path)
-sys.path.insert(0, repo2_path)
 
-# 모듈 임포트
-import functions as repo1_functions  # repo2/functions.py의 함수
-sys.path.remove(repo1_path)  # repo1 경로 제거 (중복 방지)
-import functions as repo2_functions  # repo1/functions.py의 함수
+repo_paths = {
+    'repo1': repo1_path,
+    'repo2': repo2_path
+}
 
+from repo2_path import functions as repo2_functions
+from repo2_path import functions as repo1_functions
 
 class TestIntegration(unittest.TestCase):
     def test_process_and_transform(self):
